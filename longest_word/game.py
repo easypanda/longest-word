19fa1c03740fdf2689d5bf486c09f2b0f7bb3c11
Alexandre Casula <casula.alexandre@gmail.com>
2024-06-06 17:40:08 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,14 +1,17 @@
 import random
 import string
+import requests
+
 class Game:
     """Docstring"""
     def __init__(self) -> list:
         """Attribute a random grid to size 9"""
         self.grid = [random.choice(string.ascii_uppercase) for _ in range(9)]
 
-
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
+        r = requests.get(f"https://dictionary.lewagon.com/{word}")
+
         lst_word = [x for x in word]
 
         for letters in self.grid:
@@ -18,6 +21,8 @@ class Game:
         if len(lst_word) > len(self.grid):
             return False
         elif len(lst_word) >= 1:
+            return False
+        elif r.json()['found'] == False:
             return False
         else:
             return True
